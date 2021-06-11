cfaf1143d6415e05611ebaacd9111e64acccb01f
Jonathan Ferreira Kuntz <jonathanfk1@gmail.com>
2021-06-11 12:18:35 -0300

    Fixing git files error on updating title via script.

--- scripts/rename_project_title.sh
+++ scripts/rename_project_title.sh
@@ -12,6 +12,11 @@ OLD_NAME_LOWER=${OLD_NAME,,}
 NEW_NAME_LOWER=${NEW_NAME,,}
 
 
+# Changes to lower case
+OLD_NAME_TESTS=$OLD_NAME'Tests'
+NEW_NAME_TESTS=$NEW_NAME'Tests'
+
+
 # Prints
 echo 'Old project name: '$OLD_NAME
 echo 'Old project name lowercase: '$OLD_NAME_LOWER
@@ -21,14 +26,14 @@ echo 'New project name lowercase: '$NEW_NAME_LOWER
 
 
 # Changes the title occourence inside every file to the new one
-find . -type f -exec sed -i 's/'$OLD_NAME_LOWER'/'$NEW_NAME_LOWER'/g' {} \;
-find . -type f -exec sed -i 's/'$OLD_NAME'/'$NEW_NAME'/g' {} \;
+find . -path ./.git -prune -false -o -type f -exec sed -i 's/'$OLD_NAME_LOWER'/'$NEW_NAME_LOWER'/g' {} \;
+find . -path ./.git -prune -false -o -type f -exec sed -i 's/'$OLD_NAME'/'$NEW_NAME'/g' {} \;
 
 
 # Changes directories and files names
 mv android/app/src/debug/java/com/$OLD_NAME_LOWER android/app/src/debug/java/com/$NEW_NAME_LOWER
 mv android/app/src/main/java/com/$OLD_NAME_LOWER android/app/src/main/java/com/$NEW_NAME_LOWER
 mv ios/$OLD_NAME/ ios/$NEW_NAME/
-mv ios/$OLD_NAMETests/$OLD_NAMETests.m ios/$OLD_NAMETests/$NEW_NAMETests.m
-mv ios/$OLD_NAMETests/ ios/$NEW_NAMETests/
+mv ios/$OLD_NAME_TESTS/$OLD_NAME_TESTS.m ios/$OLD_NAME_TESTS/$NEW_NAME_TESTS.m
+mv ios/$OLD_NAME_TESTS/ ios/$NEW_NAME_TESTS/
 mv ios/$OLD_NAME.xcodeproj ios/$NEW_NAME.xcodeproj
